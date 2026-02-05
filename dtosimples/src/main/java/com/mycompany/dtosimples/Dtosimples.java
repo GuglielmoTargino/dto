14ec6aa4756e31a60451af91a02941705f74f17d
GHT <guglielmohenriquestargino@gmail.com>
2026-02-05 15:04:35 -0300

    ok dto_p2
    Finalizada a parte 2 do DTO.

--- dtosimples/src/main/java/com/mycompany/dtosimples/Dtosimples.java
+++ dtosimples/src/main/java/com/mycompany/dtosimples/Dtosimples.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.dtosimples;
 import com.mycompany.dtosimples.entities.Calcado;
+import com.mycompany.dtosimples.dtos.VisaoCliente;
+import com.mycompany.dtosimples.dtos.VisaoEstoquista;
+
 
 /**
  *
@@ -13,9 +16,12 @@ public class Dtosimples {
 
     public static void main(String[] args) {
         
-        Calcado tenis = new Calcado("esporte", "azul", "mizuno", 126.56, 127.2, 5.6, 2.3, 500, 12, 38);
+        Calcado tenis = new Calcado("esporte","azul", "mizuno", 126.56,127.2, 5.6, 2.3, 500, 12, 38);
         
+        //VisaoCliente cliente = new VisaoCliente(tenis);
         
-        System.out.println(tenis.getEstoque());
+        VisaoEstoquista usuario = new VisaoEstoquista(tenis);
+        
+        System.out.println(usuario.getEstoque());
     }
 }
